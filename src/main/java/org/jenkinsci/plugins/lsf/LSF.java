897dd0806b56949d98acd6e9c259e50b81497aa1
john McGehee <jmcgehee@wavesemi.com>
2015-12-16 20:08:11 -0800

    Remove instrumentation from LSF.java.

--- src/main/java/org/jenkinsci/plugins/lsf/LSF.java
+++ src/main/java/org/jenkinsci/plugins/lsf/LSF.java
@@ -2,6 +2,7 @@
  * The MIT License
  *
  * Copyright 2015 Laisvydas Skurevicius.
+ * Copyright 2015 Wave Semiconductor
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -34,21 +35,19 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Pattern;
+
 import jenkins.util.BuildListenerAdapter;
 import org.apache.commons.io.FileUtils;
 
 /**
- *
- * @author Laisvydas Skurevicius
+ * Interface to the Sun Grid Engine (SGE) batch system
+ * 
+ * @author John McGehee
+ * 
+ * Based on the LSF version by Laisvydas Skurevicius
  */
 public class LSF extends BatchSystem {
-
-    private static final Set ENDING_STATES = new HashSet();
-
-    static {
-        ENDING_STATES.add("DONE");
-        ENDING_STATES.add("EXIT");
-    }
 
     private static final BuildListenerAdapter fakeListener
             = new BuildListenerAdapter(TaskListener.NULL);
@@ -59,164 +58,302 @@ public class LSF extends BatchSystem {
         super(build, launcher, listener, COMMUNICATION_FILE, 
                 masterWorkingDirectory);
     }
+    
+    private static final String OUTPUT_FILE = "sge.log";
+    
+    // When the Jenkins SGE plugin fails to even submit the job to SGE, it gives
+    // status "J", which is not a real SGE status. 
+    private static final String JENKINS_FAIL_STATUS = "J";
+
+    // When a job completes successfully, it exits with status "0". 
+    private static final String DONE_STATUS = "0";
 
     @Override
     public String submitJob(String jobFileName, boolean sendEmail,
             String queueType) throws InterruptedException, IOException {
 
-        // checks if email notifications should be sent and configures the command
-        String emailConfiguration = "";
+        // Inhibit email if requested
+        String emailOption = "";
         if (!sendEmail) {
-            emailConfiguration = "LSB_JOB_REPORT_MAIL=N ";
+            emailOption = "-m n";
         }
-        // submits the job to LSF
-        Shell shell = new Shell("#!/bin/bash +x\n" + emailConfiguration
-                + "bsub -q " + queueType + " -e \"errorLog\" "
-                + jobFileName + " | tee " + COMMUNICATION_FILE);
+        // submit the job to SGE
+        Shell shell = new Shell("#!/bin/bash +x\n" +
+                // "env | sort\n" +
+        		"rm -f " + OUTPUT_FILE + "\n" +
+                "\"$SGE_BIN/qsub\" " +
+                    emailOption +
+                    " -q " + queueType +
+                    " -N " + jobFileName +
+                    " -cwd" +
+                    " -V" +
+                    " -o " + OUTPUT_FILE +
+                    " -j yes " + // Send stderr + stdout to OUTPUT_FILE
+                    jobFileName +
+                    "  &> " + COMMUNICATION_FILE);
         shell.perform(build, launcher, listener);
 
-        // stores the job id
+        // Extract the job id
         copyFileToMaster.perform(build, launcher, fakeListener);
         BufferedReader fileReader = new BufferedReader(
                 new FileReader(masterWorkingDirectory + COMMUNICATION_FILE));
-        String jobId = fileReader.readLine();
-        jobId = jobId.substring(jobId.indexOf('<', 0)
-                + 1, jobId.indexOf('>', 0));
+        // qsub prints, 'Your job jobId ("jobName") has been submitted'
+        String line = fileReader.readLine();
+        String jobId = "0";
+        if (line == null) {
+            listener.getLogger().println(
+            		"ERROR: SGE qsub job submission failed.  There was no " +
+                    "diagnostic message.");
+        } else if (!line.startsWith("Your job ") ||
+        		!line.endsWith(" has been submitted")) {
+        	listener.getLogger().println(
+        			"ERROR: SGE qsub job submission failed because '" +
+        	        line + "'.");
+        } else {
+            jobId = line.trim().split(" ")[2];
+            listener.getLogger().println(line);
+        }
         return jobId;
     }
 
     @Override
     public String getJobStatus(String jobId)
             throws IOException, InterruptedException {
-        Shell shell = new Shell("#!/bin/bash +x\n bjobs " + jobId + " > "
-                + COMMUNICATION_FILE);
+    	if (jobId.equals("0")) {
+    		// submitJob() returns "0" when it fails to even submit to SGE
+    		return JENKINS_FAIL_STATUS;
+    	}
+    	
+    	String jobStatus = DONE_STATUS;
+        Shell shell = new Shell("#!/bin/bash +x\n" +
+        		"\"$SGE_BIN/qstat\" -u $USER > " + COMMUNICATION_FILE);
         shell.perform(build, launcher, fakeListener);
         copyFileToMaster.perform(build, launcher, fakeListener);
+        String jobStats = getValueFromFile(jobId);
+        if (jobStats != null) {
+        	String word[] = jobStats.trim().split("\\s+");
+        	return word[3];
+        }
+        
+    	// qstat did not list jobId, the job must be finished.  Get its
+    	// return status using qacct.
+    	String exitStatus = getFinishedJobExitStatus(jobId);
+    	if (exitStatus == null) {
+        	listener.getLogger().println("SGE plugin failed to get the " +
+        			"exit status for job '" + jobId + "  Assuming '" +
+        			DONE_STATUS + "', which means the job succeeded.");
+        	exitStatus = DONE_STATUS;
+    	}
+    	return exitStatus;
+    }
+
+    private String getFinishedJobExitStatus(String jobId)
+            throws IOException, InterruptedException {
+    	Shell shell = new Shell("#!/bin/bash +x\n \"$SGE_BIN/qacct\" -j " + 
+            jobId + " > " + COMMUNICATION_FILE);
+    	shell.perform(build, launcher, fakeListener);
+    	copyFileToMaster.perform(build, launcher, fakeListener);
+    	
+    	String exitStatus = getValueFromFile("exit_status");
+    	return exitStatus;
+    }
+    
+    /** Presuming that COMMUNICATION_FILE already contains the result of
+     * `qacct`or `qstat`, open it and return the value for the specified
+     * key.  The file is presumed to be in the format::
+     * 
+     *      key1     value1
+     *      key2     value    containing    white  space
+     *          ...
+     *
+     * Repeated white space in the value will be normalized to a single space.
+     * For example, getValueFromFile("key2") will return "value containing white
+     * space".
+     * 
+     * If key is not found, return `null`.
+     */
+    private String getValueFromFile(String key)
+            throws IOException, InterruptedException {
         BufferedReader fileReader = new BufferedReader(
                 new FileReader(masterWorkingDirectory + COMMUNICATION_FILE));
-        fileReader.readLine();
-        return fileReader.readLine().trim().split(" ")[2];
+        String value = null;
+        String line;
+        while ((line = fileReader.readLine()) != null) {
+        	String word[] = line.trim().split("\\s+");
+        	if (word[0].equals(key)) {
+        		value = word[1];
+        		for (int i = 2; i < word.length; i++) {
+        			value += " ";
+        			value += word[i];
+        		}
+        		break; 
+        	}
+        }
+        fileReader.close();
+        return value;
     }
 
     @Override
     public void killJob(String jobId) throws InterruptedException {
-        Shell shell = new Shell("#!/bin/bash +x\n bkill " + jobId);
+        Shell shell = new Shell("#!/bin/bash +x\n" +
+        		"\"$SGE_BIN/qdel\" " + jobId);
         shell.perform(build, launcher, listener);
     }
 
     @Override
     public void processStatus(String jobStatus) {
-        if (jobStatus.equals("PEND")) {
-            listener.getLogger().println("Waiting in a queue for scheduling "
-                    + "and dispatch.");
-        } else if (jobStatus.equals("RUN")) {
-            listener.getLogger().println("Dispatched to a host and running.");
-        } else if (jobStatus.equals("DONE")) {
-            listener.getLogger().println("Finished normally with zero "
-                    + "exit value.");
-        } else if (jobStatus.equals("EXIT")) {
-            listener.getLogger().println("Finished with non-zero exit value.");
-        } else if (jobStatus.equals("PSUS")) {
-            listener.getLogger().println("Suspended while pending.");
-        } else if (jobStatus.equals("USUS")) {
-            listener.getLogger().println("Suspended by user.");
-        } else if (jobStatus.equals("SSUS")) {
-            listener.getLogger().println("Suspended by the LSF system.");
-        } else if (jobStatus.equals("WAIT")) {
-            listener.getLogger().println("Members of a chunk job that "
-                    + "are waiting to run.");
+    	/**
+    	 * Print an explanation for the given job status.
+		 * 
+		 * These explanations of status come from the qstat man page.
+    	 */
+        if (jobStatus.equals("d")) {
+            listener.getLogger().println("A qdel command has been used to "
+            		+ "initiate deletion of this job.");
+        } else if (jobStatus.equals("t")) {
+            listener.getLogger().println("This job is transferring and about "
+            		+ "to be executed.");
+        } else if (jobStatus.equals("r")) {
+            listener.getLogger().println("This job is executing.");
+        } else if (jobStatus.equals("s")) {
+            listener.getLogger().println("This job was suspended by the user "
+            		+ "via the qmod command.");
+        } else if (jobStatus.equals("S")) {
+            listener.getLogger().println("The queue containing this job is "
+            		+ "suspended and therefore this job is also suspended.");
+        } else if (jobStatus.equals("T")) {
+            listener.getLogger().println("This job was suspended because at "
+            		+ "least one suspend threshold of its queue has been "
+            		+ "exceeded.");
+        } else if (jobStatus.equals("R")) {
+            listener.getLogger().println("This job has been restarted.  "
+            		+ "This can be caused by a job migration or because of one "
+            		+ "of the reasons described in the -r section of the qsub "
+            		+ "command.");
+        } else if (jobStatus.equals("w")) {
+            listener.getLogger().println("This job is waiting pending execution.");
+        } else if (jobStatus.equals("h")) {
+            listener.getLogger().println("This job is not eligible for "
+            		+ "execution because it has been assigned a hold state "
+            		+ "via qhold, qalter or the qsub -h option "
+            		+ "or the job is waiting for completion of the jobs "
+            		+ "to which job dependencies have been assigned "
+            		+ "via the -hold_jid or -hold_jid-ad options of "
+            		+ "qsub or qalter.");
+        } else if (jobStatus.equals("E")) {
+            listener.getLogger().println("This job could not be started due to "
+            		+ "job properties.  The reason for the error is shown by "
+            		+ "the qstat -j job_list option.");
+        } else if (jobStatus.equals(JENKINS_FAIL_STATUS)) {
+            listener.getLogger().println("The Jenkins SGE plugin failed to even "
+            		+ "submit this job to SGE.");
+        } else if (jobStatus.equals(DONE_STATUS)) {
+            listener.getLogger().println("This job has completed successfully.");
+        } else if (jobExitedWithErrors(jobStatus)) {
+            listener.getLogger().println("The job failed with exit status '"
+                    + jobStatus + "'.");
         } else {
-            listener.getLogger().println("Job status not recognized.");
+            listener.getLogger().println("Job status '" + jobStatus
+            		+ "' is not recognized.");
         }
     }
 
     @Override
     public void printErrorLog() throws InterruptedException {
         listener.getLogger().println("Job exited with following errors:");
-        Shell shell = new Shell("#!/bin/bash +x\n cat errorLog");
+        Shell shell = new Shell("#!/bin/bash +x\n cat " + COMMUNICATION_FILE);
         shell.perform(build, launcher, listener);
     }
 
     @Override
     public void printExitCode(String jobId)
             throws InterruptedException, IOException {
-        Shell shell = new Shell("#!/bin/bash +x\n bjobs -l "
-                + jobId + " > " + COMMUNICATION_FILE);
-        shell.perform(build, launcher, fakeListener);
-        copyFileToMaster.perform(build, launcher, fakeListener);
-        String exitCode = FileUtils.readFileToString(
-                new File(masterWorkingDirectory + COMMUNICATION_FILE));
-        if (exitCode.contains("Exited with exit code ")) {
-            listener.getLogger().println();
-            exitCode = exitCode.substring(
-                    exitCode.indexOf("Exited with exit code "),
-                    exitCode.length());
-            exitCode = exitCode.substring(0, exitCode.indexOf(".") + 1);
-            listener.getLogger().println(exitCode);
+        String exitStatus = getFinishedJobExitStatus(jobId);
+        if (exitStatus != null) {
+        	listener.getLogger().println("Exited with exit status " + exitStatus);
+        } else {
+        	listener.getLogger().println(
+        			"Exited, but the exit status could not be found");
         }
     }
 
     @Override
     public void createJobProgressFile(String jobId, String outputFileName)
             throws InterruptedException, IOException {
-        Shell shell = new Shell("#!/bin/bash +x\n bpeek "
-                + jobId + " > " + outputFileName);
+        Shell shell = new Shell("#!/bin/bash +x\n" +
+                                "cp " + OUTPUT_FILE + " " + outputFileName);
         shell.perform(build, launcher, listener);
     }
 
+    /**
+     * While the job is still running, create a file on the slave that begins
+     * after the specified offset and ending with line numberOfLines
+     * (created by createJobProgressFile method)
+     *
+     * @param outputFileName the output file to be formatted
+     * @param offset number of lines that should be skipped at the beginning
+     * @param numberOfLines the last line in outputFileName to be output
+     * @throws InterruptedException
+     */
     @Override
     public void createFormattedRunningJobOutputFile(String outputFileName,
             int offset, int numberOfLines)
             throws InterruptedException, IOException {
-        // for clearing the running job output headers
-        if (offset > 2) {
-            offset = offset - 2;
-        } else {
-            offset = offset + 2;
-        }
-        numberOfLines = numberOfLines - 2;
         Shell shell = new Shell("#!/bin/bash +x\n tail -n+" + offset + " "
                 + outputFileName + " | head -n " + (numberOfLines - offset)
                 + " > " + COMMUNICATION_FILE);
         shell.perform(build, launcher, fakeListener);
     }
 
+    /**
+     * After the job is finished, create a job output file on the slave
+     * beginning with the line after the offset through the end of the file.
+     *
+     * @param jobId the identifier of the job
+     * @param offset number of lines at the beginning of the file that should
+     * be skipped
+     * @throws InterruptedException
+     */
     @Override
     public void createFinishedJobOutputFile(String jobId, int offset)
             throws InterruptedException {
         // because of the running job output headers
-        if (offset >= 3) {
-            offset = offset - 3;
-        }
         Shell shell = new Shell("#!/bin/bash +x\n tail -n+" + offset
-                + " LSFJOB_" + jobId + "/STDOUT" + " > " + COMMUNICATION_FILE);
+                + " " + OUTPUT_FILE + " > " + COMMUNICATION_FILE);
         shell.perform(build, launcher, listener);
     }
-    
+
     @Override
     public void cleanUpFiles(String jobId) throws InterruptedException {
-        Shell shell = new Shell("rm -rf LSFJOB_" + jobId + " errorLog");
+        Shell shell = new Shell("rm -rf JOB-*-*-*-*-*");
         shell.perform(build, launcher, fakeListener);
     }
 
     @Override
     public boolean isRunningStatus(String jobStatus) {
-        return jobStatus.equals("RUN");
+        return jobStatus.equals("r");
     }
 
     @Override
     public boolean isEndStatus(String jobStatus) {
-        return ENDING_STATES.contains(jobStatus);
+        return jobCompletedSuccessfully(jobStatus) ||
+        		jobExitedWithErrors(jobStatus);
     }
 
     @Override
     public boolean jobExitedWithErrors(String jobStatus) {
-        return jobStatus.equals("EXIT");
+        return  jobStatus.equals("E") ||
+        		jobStatus.equals(JENKINS_FAIL_STATUS) ||
+        		jobExitedWithNonzeroStatus(jobStatus);
     }
+
+    private boolean jobExitedWithNonzeroStatus(String jobStatus) {
+    	return !jobStatus. equals("0") && jobStatus.matches("\\d+");
+	}
 
     @Override
     public boolean jobCompletedSuccessfully(String jobStatus) {
-        return jobStatus.equals("DONE");
+        return jobStatus.equals(DONE_STATUS);
     }
 
 }
