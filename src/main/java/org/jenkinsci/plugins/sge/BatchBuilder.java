4dc4d808a6fcb36e1c41fea2a9e1fb02a206136f
John McGehee <john.mcgehee@mediatek.com>
2020-06-10 19:09:21 -0700

    Migrate documentation from the Jenkins wiki

--- src/main/java/org/jenkinsci/plugins/sge/BatchBuilder.java
+++ src/main/java/org/jenkinsci/plugins/sge/BatchBuilder.java
@@ -2,7 +2,7 @@
  * The MIT License
  *
  * Copyright 2015 Laisvydas Skurevicius.
- * Copyright 2019 Wave Computing, John MdVehee
+ * Copyright 2019 Wave Computing, John McGehee
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
